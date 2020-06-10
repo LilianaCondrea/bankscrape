a132b795b9cdd8b460f4e9d77bbbeeb4d39b517f
LilianaCondrea <l.cndrea@yahoo.com>
2020-06-10 19:27:00 +0300

    update rspec

--- bendigobank_spec.rb
+++ bendigobank_spec.rb
@@ -4,16 +4,48 @@ require 'rspec'
 require 'Nokogiri'
 require_relative 'account'
 require_relative 'transaction'
-describe Account do
-  html     = Nokogiri::HTML.fragment(browser.div(class: 'accounts-list').html)
-  accounts = Account.parse_accounts(html)
-  expect(accounts.count).to eq(2)
-  expect(accounts[0].to_hash).to eq(
-    {
-      name: item.find_element(class: 'grouped-list__group__heading').text,
-      balance: item.find_element(class: '_1vKeQVO7xz S3CFfX95_8').text,
-      nature: item.find_element(class: 'visuallyhidden').span
-    }
-  )
 
-end
+html_account = Nokogiri::HTML(File.read('accounts.html'))
+html_transaction = Nokogiri::HTML(File.read('transactions.html'))
+accounts = Bendigobank.parse_accounts(html_account)
+transactions = Bendigobank.parse_transactions(html_transaction,"account_name")
+
+describe "accounts counting" do
+    it 'if number of accounts is 5 returns true' do
+      expect(accounts.count).to eq(5)
+    end
+  end
+
+  describe "transactions counting" do
+    it 'returns true if number of transctions is 15' do
+      expect(transactions.count).to eq(15)
+    end
+  end
+
+  describe " account validation " do
+    it 'returns true if accounts[0] is valid' do
+      expect(accounts.first.to_h).to eq(
+        {
+          "name"=>"Justin Nguyen",
+          "currency"=>"$",
+          "balance"=>1959.90,
+          "nature"=>"card",
+          "transactions"=>[]
+        }
+      )
+    end
+  end
+
+  describe "transaction validation" do
+    it 'returns true if transactions[0] is valid' do
+      expect(transactions.first.to_h).to eq(
+        {
+          "date"=>"May 27, 2020",
+          "description"=>"00001792196502",
+          "amount"=> 15000.00,
+          "currency"=>"$",
+          "account_name"=>"account_name"
+        }
+      )
+    end
+ end
